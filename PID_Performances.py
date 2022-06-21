1c44097797cea502ded8ca975c483f57f9550417
Giulia Marchiori Pietrosanti <48836873+GiuliaMP@users.noreply.github.com>
2022-06-21 21:59:06 +0200

    small changes.

--- PID_Performances.py
+++ PID_Performances.py
@@ -1,30 +1,30 @@
 from cmath import inf
 import numpy as np
 
-
 def rise_time(x, ref, time):
     """Time point at which the output signal crosses the desired reference value"""
     i = 0
     for x_val in x:
-        if (x_val + 0.001) >= ref:
-            return time[i]
+        if (x_val >= ref[i]):
+            return round(time[i],3)
         i+=1
     return inf
 
 def overshoot(x, ref):
     """Difference between the max value of the system output and the desired reference value"""
-    if max(x) >= ref:
-        return round(max(x)-ref, 3)
+    ind = np.where(x == np.max(x))
+    if x[ind][0] >= ref[ind][0]:
+        return round(x[ind][0]-ref[ind][0], 3)
     return 0 
 
 def steady_state_error(x, ref):
     """Difference between steady state value of the output signal and value of the reference signal"""
-    return (x[-1] - ref)
+    return round(np.abs(x[-1] - ref[-1]),3)
 
 
-def settling_time(x, ref, time):
+def settling_time(x, time):
     """Time at which the output reaches its steady state value"""
     i = 0
-    while x[i] > x[-1] + 0.0001 or x[i] < x[-1] - 0.0001:
+    while x[i] > x[-1] + 0.001 or x[i] < x[-1] - 0.001:
         i+=1
-    return time[i]
+    return round(time[i],3)
